d2157385ddc02d2e7776726a1f15ae045b789c56
Krzysztof Czołczyński <43497626+Kszymhu@users.noreply.github.com>
2023-01-13 12:11:08 +0100

    Create TupleOperationsTests.cs

---
+++ tests/TheRayTracerChallenge.Tests/TupleOperationsTests.cs
@@ -0,0 +1,19 @@
+using TheRayTracerChallenge.FloatMath;
+
+namespace TheRayTracerChallenge.Tests
+{
+    public class TupleOperationsTests
+    {
+        // v.Magnitude
+        // f * v, v * f
+        // v / f
+        // v + v
+        // v - v
+        // v dot v
+        // v cross v
+        // 
+        // p - p
+        // p + v
+        // p - v
+    }
+}
